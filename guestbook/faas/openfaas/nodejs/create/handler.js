4889c12aa4bcf7b56aa1f2b941e67324eabdfebd
Pete Johnson <petecj2@gmail.com>
2018-10-01 14:25:50 -0400

    Simplified code in the modified node8-express template and submitted a PR back to them

--- guestbook/faas/openfaas/nodejs/create/handler.js
+++ guestbook/faas/openfaas/nodejs/create/handler.js
@@ -6,44 +6,55 @@ const MongoClient = mongodb.MongoClient;
 const url = 'mongodb://fonkdb-mongodb.default:27017/guestbook_app';
 
 module.exports = (event, context) => {
-
     var data = {};
-    data.updatedAt = new Date().getTime();
-    if (typeof event.body == 'string') // String or obj request body?
-      data.text = JSON.parse(event.body).text; // Command line sends text
-    else
-      data.text = event.body.text; // API gateway sends obj
-    console.log('Attempting pass: ' + data.text);
 
-    var createPromise = new Promise((resolve, reject) => {
-      MongoClient.connect(url, (err, db) => {
-        if (err) {
-          reject(err);
-        } else {
+    // First, see if we have an OPTIONS call
+    if (event.method == 'OPTIONS') {
+      context
+          .status(200)
+          .headers({ 'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With' })
+          .succeed(data);
+    } else {
+      // Nope, it's a POST, continue processing
+      data.updatedAt = new Date().getTime();
+      if (typeof event.body == 'string') // String or obj request body?
+        data.text = JSON.parse(event.body).text; // Command line sends text
+      else
+        data.text = event.body.text; // API gateway sends obj
+      console.log('Attempting pass: ' + data.text);
 
-          db.collection('entries').insertOne(data, (errInsert) => {
-            if (errInsert) {
-              reject(errInsert);
-            } else {
-              resolve(data);
-              db.close();
-            }
-          });
+      var createPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, (err, db) => {
+          if (err) {
+            reject(err);
+          } else {
 
-        }
+            db.collection('entries').insertOne(data, (errInsert) => {
+              if (errInsert) {
+                reject(errInsert);
+              } else {
+                resolve(data);
+                db.close();
+              }
+            });
+
+          }
+        });
       });
-    });
-    createPromise
-     .then(function (fulfilled) {
-        context
-            .status(200)
-            .headers({ 'Access-Control-Allow-Origin': '*' })
-            .succeed(data);
-      })
-     .catch(function (error) {
-        context
-            .status(500)
-            .headers({ 'Access-Control-Allow-Origin': '*' })
-            .succeed(data);
-      });
+      createPromise
+       .then(function (fulfilled) {
+          context
+              .status(200)
+              .headers({ 'Access-Control-Allow-Origin': '*' })
+              .succeed(data);
+        })
+       .catch(function (error) {
+          context
+              .status(500)
+              .headers({ 'Access-Control-Allow-Origin': '*' })
+              .succeed(data);
+        });
+    }
   };
--- guestbook/faas/openfaas/nodejs/template/node8-express/index.js
+++ guestbook/faas/openfaas/nodejs/template/node8-express/index.js
@@ -81,12 +81,7 @@ var middleware = (req, res) => {
 
 app.post('/', middleware);
 app.get('/', middleware);
-app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.send(200);
-});
+app.options('/', middleware);
 
 const port = process.env.http_port || 3000;
 
