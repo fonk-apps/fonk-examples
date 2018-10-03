419d350e699480213a067a69622caee0b2888fd6
Julio Gomez <juliogomezsanchez@gmail.com>
2018-10-03 12:44:12 +0200

    Guestbook on OpenFaas/Python

---
+++ guestbook/faas/openfaas/python/template/python27-flask/index.py
@@ -0,0 +1,16 @@
+# Copyright (c) Alex Ellis 2017. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+from flask import Flask, request
+from function import handler
+
+app = Flask(__name__)
+
+@app.route("/", defaults={"path": ""}, methods=["POST", "GET"])
+@app.route("/<path:path>", methods=["POST", "GET"])
+def main_route(path):
+    ret = handler.handle(request.get_data())
+    return ret
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
